01fec94a0ba81011d1c269b0a58b79ecde472c63
Nikola <n.maric1912@gmail.com>
2024-02-03 15:01:05 +0100

    feat:add setters in loader plugins #11

--- core/src/use_cases/load.py
+++ core/src/use_cases/load.py
@@ -1,20 +1,21 @@
 import pkg_resources
 from django.apps import AppConfig
 
-def load_plugins(group):
-        plugins = []
+def load_plugins(group) -> dict:
+        plugins = {}
+        
         for ep in pkg_resources.iter_entry_points(group=group):
             p = ep.load()
             print("{} {}".format(ep.name, p))
             plugin = p()
-            plugins.append(plugin)
+            plugins[plugin.identifier()] = plugin
         return plugins
 
 class Loader():
 
     def __init__(self):
-        self.data_sources = []
-        self.visualizers = []    
+        self.data_sources = {}
+        self.visualizers = {}
     
     def load_data_sources(self):
         self.data_sources = load_plugins('graph.load')
@@ -24,8 +25,16 @@ class Loader():
 
     @property
     def data_sources(self): 
-         return self.data_sources 
+        return self._data_sources 
+    
+    @data_sources.setter
+    def data_sources(self, value):
+        self._data_sources = value
     
     @property
     def visualizers(self): 
-         return self.visualizers
+        return self._visualizers
+    
+    @visualizers.setter
+    def visualizers(self, value):
+        self._visualizers = value
