c72f639b8455fb495e23cd67eefc3a2c663bd2e4
pejinovics <pejinovicstefan@gmail.com>
2024-02-09 11:51:46 +0100

    feat: add search function #28

--- core/src/use_cases/main.py
+++ core/src/use_cases/main.py
@@ -9,10 +9,23 @@ class Core:
         self.loader: Loader = Loader()
         self.loader.load_data_sources()
         self.loader.load_visualizers()
+        self.current_visualizer: str = ""
         
     
     def load_main_view(self,  data_source_key:str, visualizer_key:str):
         data_source = self.loader.data_sources[data_source_key]
         self.graph = data_source.load_graph()
+        self.current_visualizer = visualizer_key
         retVal = self.loader.visualizers[visualizer_key].show_graph(self.graph)
         return retVal
+
+    def filter_graph(self, searchText, key, value, comparison_operator):
+        retVal = self.loader.visualizers[self.current_visualizer].show_graph(self.graph)
+        isValid = self.graph.filter(key, value, comparison_operator)
+
+        if not isValid:
+            return retVal, False
+
+        self.graph.search(searchText)
+        retVal = self.loader.visualizers[self.current_visualizer].show_graph(self.graph)
+        return retVal, isValid
