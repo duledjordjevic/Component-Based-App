1046b1d2d8456f8e8b19218cdedf6895788ecaac
pejinovics <pejinovicstefan@gmail.com>
2024-02-09 11:50:34 +0100

    feat: add path to filter #28

--- graph_explorer/app/urls.py
+++ graph_explorer/app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("mainView", views.show_main_view, name="mainView"),
-    path('get_nodes/<int:id>/', views.get_nodes, name='get_nodes')
+    path('get_nodes/<int:id>/', views.get_nodes, name='get_nodes'),
+    path("filter", views.filter, name="filter")
 ]
